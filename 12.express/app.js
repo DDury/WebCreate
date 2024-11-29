7917ac622207613957443b6cd52c7179f8f86910
DDury <87566183+DDury@users.noreply.github.com>
2024-11-29 23:07:11 +0900

    EJS!!!

--- 12.express/app.js
+++ 12.express/app.js
@@ -2,28 +2,37 @@ const exp = require("constants");
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const path = require("path");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
-
-const path = require("path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 const adrs = ["index", "restaurants", "recommend", "about", "confirm"];
-const resData = ["name", "address", "cuisine", "website", "description"];
+
+// for (const ad of adrs) {
+//   app.get(`/${ad}`, function (req, res) {
+//     const htmlpath = path.join(__dirname, "frontend", "views", `${ad}.html`);
+//     res.sendFile(htmlpath);
+//   });
+// }
 
 for (const ad of adrs) {
   app.get(`/${ad}`, function (req, res) {
-    const htmlpath = path.join(__dirname, "frontend", "view", `${ad}.html`);
-    res.sendFile(htmlpath);
+    const jspath = path.join(__dirname, "data.json");
+    const jsdata = fs.readFileSync(jspath);
+    const exdata = JSON.parse(jsdata);
+    res.render(`${ad}`, { numberOfRestaurants: exdata.length });
   });
 }
 
 app.post("/recommend", function (req, res) {
-  const resname = req.body;
+  const resData = req.body;
   const jspath = path.join(__dirname, "data.json");
   const jsdata = fs.readFileSync(jspath);
   const exdata = JSON.parse(jsdata);
-  exdata.push(resname);
+  exdata.push(resData);
   fs.writeFileSync(jspath, JSON.stringify(exdata));
   res.redirect("/confirm");
 });
