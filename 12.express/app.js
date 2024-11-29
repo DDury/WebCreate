1d94950363b25d7e7157c6283f654ac02f779432
DDury <87566183+DDury@users.noreply.github.com>
2024-11-29 18:17:28 +0900

    JSON_body

--- 12.express/app.js
+++ 12.express/app.js
@@ -1,8 +1,31 @@
+const exp = require("constants");
 const express = require("express");
 const app = express();
+const fs = require("fs");
 
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!!!</h1>");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+
+const path = require("path");
+
+const adrs = ["index", "restaurants", "recommend", "about", "confirm"];
+const resData = ["name", "address", "cuisine", "website", "description"];
+
+for (const ad of adrs) {
+  app.get(`/${ad}`, function (req, res) {
+    const htmlpath = path.join(__dirname, "frontend", "view", `${ad}.html`);
+    res.sendFile(htmlpath);
+  });
+}
+
+app.post("/recommend", function (req, res) {
+  const resname = req.body;
+  const jspath = path.join(__dirname, "data.json");
+  const jsdata = fs.readFileSync(jspath);
+  const exdata = JSON.parse(jsdata);
+  exdata.push(resname);
+  fs.writeFileSync(jspath, JSON.stringify(exdata));
+  res.redirect("/confirm");
 });
 
 app.listen(3000);
