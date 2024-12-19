815982d421bbadc2b2d2047090d843425e00b666
DDury <87566183+DDury@users.noreply.github.com>
2024-12-19 18:29:33 +0900

    sync mysql

--- 14.mysql/app.js
+++ 14.mysql/app.js
@@ -2,7 +2,7 @@ const path = require("path");
 
 const express = require("express");
 
-// const blogRoutes = require("./routes/blog");
+const blogRoutes = require("./routes/blog");
 
 const app = express();
 
@@ -13,7 +13,7 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
 app.use(express.static("public")); // Serve static files (e.g. CSS files)
 
-// app.use(blogRoutes);
+app.use(blogRoutes);
 
 app.use(function (error, req, res, next) {
   // Default error handling function
