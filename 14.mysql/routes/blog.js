4ebaa99d12ccd9a8225922d794622314c1ca198b
DDury <87566183+DDury@users.noreply.github.com>
2024-12-20 15:41:58 +0900

    /:id req.params.id

--- 14.mysql/routes/blog.js
+++ 14.mysql/routes/blog.js
@@ -9,7 +9,10 @@ router.get("/", function (req, res) {
 });
 
 router.get("/posts", async function (req, res) {
-  const [postings] = await db.query("select * from posts");
+  const query = `select posts.* ,authors.name as author_name from posts 
+    INNER join authors on authorid = authors.id`;
+
+  const [postings] = await db.query(query);
   res.render("posts-list", { postings: postings });
 });
 
@@ -30,6 +33,15 @@ router.post("/posts", async function (req, res) {
 router.get("/new-post", async function (req, res) {
   const [authors] = await db.query("select * from authors");
   res.render("create-post", { authors: authors });
+});
+
+router.get("/post-detail/:id", async function (req, res) {
+  const query = `select * from posts 
+    INNER join authors on authorid = authors.id
+    where posts.id = ?`;
+
+  const [post] = await db.query(query, [req.params.id]);
+  res.render("post-detail", { post: post[0] });
 });
 
 module.exports = router;
