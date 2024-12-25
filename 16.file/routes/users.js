b3be982179b9d9a71004e3402aeeeeb3d1324e56
DDury <87566183+DDury@users.noreply.github.com>
2024-12-25 22:11:38 +0900

    file mongoDB

--- 16.file/routes/users.js
+++ 16.file/routes/users.js
@@ -1,13 +1,37 @@
-const express = require('express');
+const express = require("express");
+const db = require("../data/database");
+const multer = require("multer");
+
+const storageConfig = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "images");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const upload = multer({ storage: storageConfig });
 
 const router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('profiles');
+router.get("/", async function (req, res) {
+  const users = await db.getDb().collection("users").find().toArray();
+  res.render("profiles", { users: users });
 });
 
-router.get('/new-user', function(req, res) {
-  res.render('new-user');
+router.get("/new-user", function (req, res) {
+  res.render("new-user");
+});
+
+router.post("/profiles", upload.single("image"), async function (req, res) {
+  const uploadimage = req.file;
+  const contents = req.body;
+  await db.getDb().collection("users").insertOne({
+    name: contents.username,
+    imagepath: uploadimage.path,
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
