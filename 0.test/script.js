c543190ddc1cb01b8f52954adffe8d63815929ea
DDury <87566183+DDury@users.noreply.github.com>
2025-01-10 17:40:44 +0900

    slidepanel final

--- 0.test/script.js
+++ 0.test/script.js
@@ -1,14 +1,14 @@
-// DOM 요소 참조
+// DOM 요소 가져오기
 const openBtn = document.getElementById("openPanelBtn");
 const closeBtn = document.getElementById("closePanelBtn");
 const slidePanel = document.getElementById("slidePanel");
 
-// 패널 열기
+// 슬라이드 패널 열기
 openBtn.addEventListener("click", () => {
   slidePanel.classList.add("open");
 });
 
-// 패널 닫기
+// 슬라이드 패널 닫기
 closeBtn.addEventListener("click", () => {
   slidePanel.classList.remove("open");
 });
