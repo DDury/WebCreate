d4c8cf9245c26b8ae398d38a075b5e5e82c6a5ce
DDury <87566183+DDury@users.noreply.github.com>
2025-01-27 07:48:47 +0900

    deployment

--- 21.publish/models/like.model.js
+++ 21.publish/models/like.model.js
@@ -1,12 +1,12 @@
-const db = require('../data/database');
+const db = require("../data/database");
 
 class Like {
   static getLikes() {
-    return db.getDb().collection('likes').find().sort({ date: -1 }).toArray();
+    return db.getDb().collection("likes").find().sort({ date: -1 }).toArray();
   }
 
   save() {
-    return db.getDb().collection('likes').insertOne({ date: new Date() });
+    return db.getDb().collection("likes").insertOne({ date: new Date() });
   }
 }
 
